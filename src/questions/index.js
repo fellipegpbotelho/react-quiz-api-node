64d848aa14b7efe89ec619b7365e5b47743938a5
fellipegpbotelho <fellipegpbotelho@gmail.com>
2018-05-14 15:06:13 -0300

    Criação de resposta em uma pergunta existente

--- src/questions/index.js
+++ src/questions/index.js
@@ -4,12 +4,14 @@ import list from './list'
 import create from './create'
 import remove from './remove'
 import removeAnswers from './removeAnswers'
+import createAnswer from './createAnswer'
 
 const router = express.Router()
 
 router.get('/', list)
 router.post('/', create)
 router.delete('/:id/answers/:idAnswer', removeAnswers)
+router.post('/:id/answers', createAnswer)
 router.delete('/:id', remove)
 
 export default router
