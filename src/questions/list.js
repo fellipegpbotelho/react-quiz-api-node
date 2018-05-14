98b2f37952bd10572d6ddc08093acc83ea48dd8d
fellipegpbotelho <fellipegpbotelho@gmail.com>
2018-05-14 15:16:09 -0300

    Finalizando todos os recursos da api

--- src/questions/list.js
+++ src/questions/list.js
@@ -1,11 +1,8 @@
 import Question from './../models/Question'
 
 export default async (req, res) => {
-
-  let questions = []
-
   try {
-    questions = await Question.find({})
+    let questions = await Question.find({}).populate('subject')
     return res.json({ questions })
   } catch (error) {
     return res.status(500).json({ error })        
--- src/subjects/list.js
+++ src/subjects/list.js
@@ -5,7 +5,7 @@ export default async (req, res) => {
   let subjects = []
 
   try {
-    subjects = await Subject.find({})
+    subjects = await Subject.find({}).populate('questions')
     return res.json({ subjects })
   } catch (error) {
     return res.status(500).json({ error })        
